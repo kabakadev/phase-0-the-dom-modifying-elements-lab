797386e76d5ba5353059bd344a9fbd86a37c4bf9
Ian Kabaka <iankabaka1@gmail.com>
2024-09-19 09:09:59 +0300

    modifies the DOM,appends to the body

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const removeMain = document.querySelector('main#main')
+removeMain.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Ian Kabaka is the champion"
+document.body.append(newHeader)
